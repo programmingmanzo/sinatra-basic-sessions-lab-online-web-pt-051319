1a787fe00735f0a6410ccbaeeffbeb5e6b03161f
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-26 04:16:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+    configure do 
+        enable :sessions
+        set :session_secret, "atestsecretthatshouldbemoresecure"
+    end
+
+    get '/' do
+        erb :index
+    end 
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+
+        erb :checkout
+    end
 end
